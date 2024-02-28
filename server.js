e82f3cde315c9492d601da6d1244864712354afd
arseniy54 <91014197+arseniy54@users.noreply.github.com>
2024-02-28 14:55:56 +0300

    r
    r

--- server.js
+++ server.js
@@ -237,6 +237,6 @@ app.delete('/cards/:cardId', async (req, res) => {
     }
   });
 });
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+app.listen(5000, () => {
+  console.log('Server is running on port 5000');
 });
